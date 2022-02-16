41acef3892e7f69dd47852c30bba5f7c341a1e2a
Salam Baalbaki <salambaalbaki@gmail.com>
2022-02-16 16:03:59 +0400

    built a two model rails app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # A visitor can see the list of all restaurants.
+  # GET "restaurants" --> index
+
+  # A visitor can add a new restaurant, and be redirected to the show view of that new restaurant. --> show
+  # GET "restaurants/new" --> new
+  # POST "restaurants" --> create
+
+
+  # A visitor can see the details of a restaurant, with all the reviews related to the restaurant.
+  # GET "restaurants/38" --> index
+
+  # A visitor can add a new review to a restaurant
+  # GET "restaurants/38/reviews/new" --> new
+  # POST "restaurants/38/reviews" --> create
+
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:create]
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(
+  name: 'Zuma',
+  address: 'DIFC',
+  category: 'japanese'
+)
+
+Restaurant.create(
+  name: 'LPM',
+  address: 'DIFC',
+  category: 'french'
+)
+
+Restaurant.create(
+  name: 'Belgian Cafe',
+  address: 'Tecom',
+  category: 'belgian'
+)
+
+Restaurant.create(
+  name: 'Roka',
+  address: 'Business Bay',
+  category: 'japanese'
+)
+
+Restaurant.create(
+  name: 'Chinatown',
+  address: 'Business Bay',
+  category: 'japanese'
+)
